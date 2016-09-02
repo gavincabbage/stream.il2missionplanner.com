43a82480bf78a04a15eee71180bb82ac08164eb6
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-09-02 13:18:59 -0400

    updated newStream

--- lua/newStream.lua
+++ lua/newStream.lua
@@ -3,10 +3,15 @@ local password = ARGV[2]
 local code = ARGV[3]
 local state = ARGV[4]
 
+local function setupPRNG()
+    math.randomseed(os.time())
+    math.random(); math.random(); math.random()
+end
+
 local function buildChannelName(streamName)
     local s = streamName
-    for i = 1, 8 do
-        s = s .. string.char(math.random(48, 57))
+    for i = 1, 16 do
+        s = s .. ':' .. string.char(math.random(48, 57))
     end
     return s
 end
@@ -19,7 +24,11 @@ if redis.call('EXISTS', 'stream:' .. streamName) ~= 0 then
     return 2
 end
 
+setupPRNG()
 local channel = buildChannelName(streamName)
+if not channel
+    return 3
+end
 
 redis.call('HMSET', 'stream:' .. streamName, 'name', streamName, 'pw', password,
         'code', code, 'channel', channel, 'state', state)
