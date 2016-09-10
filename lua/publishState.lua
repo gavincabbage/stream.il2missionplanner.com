faac88331d58859815e6816bff05481398a64350
Gavin Cabbage <gavin.cabbage@capitalone.com>
2016-09-10 15:22:12 -0400

    update publishState to new pattern

--- lua/publishState.lua
+++ lua/publishState.lua
@@ -4,22 +4,22 @@ local code = ARGV[3]
 local state = ARGV[4]
 
 if not streamName or not password or not code or not state then
-    return 1
+    return {'ERROR', 'All fields are required. Please try again'}
 end
 
 if redis.call('EXISTS', 'stream:' .. streamName) == 0 then
-    return 2
+    return {'ERROR', 'Stream does not exist.'}
 end
 
 local streamKey = 'stream:' .. streamName
 local fields = redis.call('HMGET', streamKey, 'pw', 'code', 'channel')
 
 if password ~= fields[1] or code ~= fields[2] then
-    return 3
+    return {'ERROR', 'Password or leader code incorrect.'}
 end
 
 redis.call('HSET', streamKey, 'state', state)
 redis.call('PUBLISH', fields[3], state)
 redis.call('EXPIRE', streamKey, 10800)
 
-return 0
+return {'SUCCESS'}
